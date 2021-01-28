5a586d1821add4c774c594b0556b5c477c02420e
nkangi jafari <nkangijafari@gmail.com>
2021-01-28 22:59:58 +0400

    Added the cart payment method action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
 	CART_ADD_ITEM,
 	CART_REMOVE_ITEM,
+	CART_SAVE_PAYMENT_METHOD,
 	CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants';
 
@@ -46,6 +47,17 @@ export const removeFromCart = (productId) => async (dispatch, getState) => {
 export const saveShippingAddress = (data) => async (dispatch) => {
 	dispatch({
 		type: CART_SAVE_SHIPPING_ADDRESS,
+		payload: data,
+	});
+
+	localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
+
+/*-------------------------------------------------------------*/
+//Save Payment Methods
+export const saveShippingAddress = (data) => async (dispatch) => {
+	dispatch({
+		type: CART_SAVE_PAYMENT_METHOD,
 		payload: data,
 	});
 
