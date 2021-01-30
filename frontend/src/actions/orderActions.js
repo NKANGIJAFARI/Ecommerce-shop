987ce59ef4b009aed46c7852ed03329f68e6b278
nkangi jafari <nkangijafari@gmail.com>
2021-01-30 14:38:49 +0400

    Added an orderAcions in the frontend

---
+++ frontend/src/actions/orderActions.js
@@ -0,0 +1,46 @@
+import {
+	ORDER_CREATE_REQUEST,
+	ORDER_CREATE_SUCCESS,
+	ORDER_CREATE_FAIL,
+} from '../constants/orderConstants';
+
+export const createNewOrder = (order) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDER_CREATE_REQUEST,
+		});
+
+		/*Destructure userInfo from the userLogin state so that we get 
+        the token from that user info */
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		//Then the config, specify the type of content to send
+		//from the body and the token because its a protected route at the backend
+		const config = {
+			headers: {
+				'content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		//Make the post request to the order API
+		const { data } = await axios.post(`/api/orders`, order, config);
+
+		//If the post request is successful, data will be filled with the response
+		dispatch({
+			type: ORDER_CREATE_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		//When the post request fails, we shall dispatch failure
+		dispatch({
+			type: ORDER_CREATE_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
