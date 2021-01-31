6dfdfc5a87cef3bcb3e37be06fe665e6b6137568
nkangi jafari <nkangijafari@gmail.com>
2021-01-31 19:19:37 +0400

    Added the order payment action/functionality to update order data in DB whenever order is paid.

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -7,6 +7,9 @@ import {
 	ORDER_DETAILS_REQUEST,
 	ORDER_DETAILS_SUCCESS,
 	ORDER_DETAILS_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS,
+	ORDER_PAY_FAIL,
 } from '../constants/orderConstants';
 
 //===========================================================================
@@ -88,6 +91,58 @@ export const getOrderDetails = (orderId) => async (dispatch, getState) => {
 		//When the post request fails, we shall dispatch failure
 		dispatch({
 			type: ORDER_DETAILS_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+//-------------------------------------------------------------------------------
+
+//===========================================================================
+//Pay order functionality
+
+export const getOrderDetails = (orderId, paymentResult) => async (
+	dispatch,
+	getState
+) => {
+	try {
+		dispatch({
+			type: ORDER_PAY_REQUEST,
+		});
+
+		/*Destructure userInfo from the userLogin state so that we get 
+        the token from that user info */
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		//Then the config, specify the type of content to send
+		//from the body and the token because its a protected route at the backend
+		const config = {
+			headers: {
+				'content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		//Make the put request to the order API
+		const { data } = await axios.put(
+			`/api/order/${orderId}/pay`,
+			paymentResult,
+			config
+		);
+
+		//If the post request is successful, data will be filled with the response
+		dispatch({
+			type: ORDER_PAY_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		//When the post request fails, we shall dispatch failure
+		dispatch({
+			type: ORDER_PAY_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
