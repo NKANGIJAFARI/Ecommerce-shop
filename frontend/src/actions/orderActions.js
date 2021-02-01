b5be17ee9fd65767f2ef571b4c6f567914bdb753
nkangi jafari <nkangijafari@gmail.com>
2021-02-01 20:43:27 +0400

    Added the get order list action/functionality for client side

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -10,6 +10,9 @@ import {
 	ORDER_PAY_REQUEST,
 	ORDER_PAY_SUCCESS,
 	ORDER_PAY_FAIL,
+	ORDERS_LIST_CLIENT_REQUEST,
+	ORDERS_LIST_CLIENT_SUCCESS,
+	ORDERS_LIST_CLIENT_FAIL,
 } from '../constants/orderConstants';
 
 //===========================================================================
@@ -143,6 +146,50 @@ export const payOrder = (orderId, paymentResult) => async (
 		//When the post request fails, we shall dispatch failure
 		dispatch({
 			type: ORDER_PAY_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+//-------------------------------------------------------------------------------
+
+//===========================================================================
+//Get all the  order list of the client side
+
+export const listClientOrders = () => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDERS_LIST_CLIENT_REQUEST,
+		});
+
+		/*Destructure userInfo from the userLogin state so that we get 
+        the token from that user info */
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		//Then the config, specify the type of content to send
+		//from the body and the token because its a protected route at the backend
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		//Make the put request to the order API
+		const { data } = await axios.get(`/api/order/myorders`, config);
+
+		//If the post request is successful, data will be filled with the response
+		dispatch({
+			type: ORDERS_LIST_CLIENT_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		//When the post request fails, we shall dispatch failure
+		dispatch({
+			type: ORDERS_LIST_CLIENT_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
