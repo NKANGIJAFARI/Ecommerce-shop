95fe3e97214bc1c01b60eaaeee468907835d5b46
nkangi jafari <nkangijafari@gmail.com>
2021-02-03 19:04:33 +0400

    Added the delete Product route and controller (Backend)

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,17 @@
 import express from 'express';
 const router = express.Router();
+import { protect, isAdmin } from '../middleware/authMiddleware';
 
 import {
 	getProducts,
 	getProductById,
+	deleteProduct,
 } from '../controllers/productController.js';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router
+	.route('/:id')
+	.get(getProductById)
+	.delete(protect, isAdmin, deleteProduct);
 
 export default router;
