3ef311ce20711e07292e3c413478e83dfe666e73
nkangi jafari <nkangijafari@gmail.com>
2021-02-06 08:19:03 +0400

    Added the order deliver actions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -16,6 +16,9 @@ import {
 	ORDERS_LIST_REQUEST,
 	ORDERS_LIST_SUCCESS,
 	ORDERS_LIST_FAIL,
+	ORDERS_DELIVER_REQUEST,
+	ORDERS_DELIVER_SUCCESS,
+	ORDERS_DELIVER_FAIL,
 } from '../constants/orderConstants';
 
 //===========================================================================
@@ -149,6 +152,54 @@ export const payOrder = (orderId, paymentResult) => async (
 		//When the post request fails, we shall dispatch failure
 		dispatch({
 			type: ORDER_PAY_FAIL,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+//-------------------------------------------------------------------------------
+
+//===========================================================================
+//Order mark as delivered functionality
+
+export const deliverOrder = (orderId) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDERS_DELIVER_REQUEST,
+		});
+
+		/*Destructure userInfo from the userLogin state so that we get 
+        the token from that user info */
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		//Then the config, specify the type of content to send
+		//from the body and the token because its a protected route at the backend
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		};
+
+		//Make the put request to the order API
+		const { data } = await axios.put(
+			`/api/order/${orderId}/deliver`,
+			{},
+			config
+		);
+
+		//If the post request is successful, data will be filled with the response
+		dispatch({
+			type: ORDERS_DELIVER_SUCCESS,
+			payload: data,
+		});
+	} catch (error) {
+		//When the post request fails, we shall dispatch failure
+		dispatch({
+			type: ORDERS_DELIVER_FAIL,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
